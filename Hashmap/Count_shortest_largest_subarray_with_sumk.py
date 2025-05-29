9a59683035d07defd5d9d6fb0e017434cd4ba0d5
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2025-05-29 11:53:08 +0530

    Added Optimizedcode using Hashmap

--- Hashmap/Count_shortest_largest_subarray_with_sumk.py
+++ Hashmap/Count_shortest_largest_subarray_with_sumk.py
@@ -48,3 +48,48 @@ for i in m1:
     if min(m1)==i:
         c=c+1
 print(c)
+
+
+"""
+Optimized Approach : WE will use Hashmap and then we will store the presum and then if max_length is hit we will check for count
+"""
+
+
+
+l=[2,3,2,3,2,1,1,1,1,1,1,1,1,8,8]
+l=[2,3,2,3,2,3,2,3,2,3,8,8]
+d={}
+c=0
+s=0
+k=8
+m=[]
+m1=[]
+max_s=0
+min_s=1000000000
+for i in range(len(l)):
+    s=s+l[i]
+    
+    if s-k in d:
+        j=d[s-k]+1
+        cur_len=i-j+1
+        if cur_len>=max_s:
+            max_s=cur_len
+            m.append(max_s)
+        if cur_len<=min_s:
+            min_s=cur_len
+            m1.append(min_s)
+        
+    if s not in d:
+        d[s]=i
+        
+print(d)
+for i in m:
+    if max(m)==i:
+        c=c+1
+print(c)
+c=0
+for i in m1:
+    if min(m1)==i:
+        c=c+1
+print(c)
+    
